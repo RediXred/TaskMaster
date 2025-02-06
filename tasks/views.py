026263c7adf1f2a225a1485abf89faf4ec78d324
RediXred <xred337@gmail.com>
2025-02-07 01:29:53 +0300

    Register/Login view added

--- tasks/views.py
+++ tasks/views.py
@@ -2,9 +2,23 @@ from django.shortcuts import render
 from .models import Task
 from .serializer import TaskSerializer
 from rest_framework import viewsets
+
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializer import RegisterSerializer, LoginSerializer
 # Create your views here.
 
 
 class TaskView(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]  # Открытый доступ
+
+class LoginView(TokenObtainPairView):
+    serializer_class = LoginSerializer
