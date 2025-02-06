07f3fc128dfb178d4fa2f1df4784c66600b6c8ac
RediXred <xred337@gmail.com>
2025-02-07 00:49:37 +0300

    created TaskView

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Task
+from .serializer import TaskSerializer
+from rest_framework import viewsets
 # Create your views here.
+
+
+class TaskView(viewsets.ModelViewSet):
+    objs = Task.objects.all()
+    serializer_class = TaskSerializer
