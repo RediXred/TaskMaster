a10111737e1382176e4ee49f9e4360f1d853b902
RediXred <xred337@gmail.com>
2025-02-07 01:56:19 +0300

    added owner for view

--- tasks/views.py
+++ tasks/views.py
@@ -15,6 +15,12 @@ class TaskView(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
