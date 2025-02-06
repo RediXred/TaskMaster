1939ce6affc423b3432b37b6ec10b7a71f4015e6
RediXred <xred337@gmail.com>
2025-02-07 00:56:01 +0300

    url fix basename

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import TaskView
 
 router = DefaultRouter()
-router.register(r"tasks", TaskView)
+router.register(r"tasks", TaskView, basename="tasks")
 
 urlpatterns = [
     path("", include(router.urls)),
