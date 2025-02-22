3469740d1aa5812f72ecbfabda331de0e69d0113
RediXred <xred337@gmail.com>
2025-02-22 20:29:09 +0300

    swagger added

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'tasks',
+    'drf_yasg',
     'rest_framework_simplejwt',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -152,3 +153,9 @@ CACHES = {
         }
     }
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0' 
+CELERY_ACCEPT_CONTENT = ['json']  
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0' 
+CELERY_TIMEZONE = 'UTC'
